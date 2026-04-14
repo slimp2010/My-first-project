3fa2ea9c69375ca52030d1079443b34d6064dc16
Carlos <nico2222bo@gmail.com>
2026-04-14 12:19:39 -0600

    Add files via upload

---
+++ practica_de_python.py
@@ -0,0 +1,15 @@
+class Employee:
+    name = str(input("What's your name? "))
+    age = int(input("How old are you: "))
+    points = int(input("How many points did you score? "))
+
+emp = Employee()
+
+print("your name is",emp.name)
+print("you're:", emp.age)
+print("You did",emp.points)
+
+if emp.points >= 17:
+    print("you've completed your work")
+else:
+    print("you need to work!")
